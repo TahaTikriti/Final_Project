d15a89b92f781acf1a828c3e5d6d7455acfcd27d
Taha Tekriti <tekrititaha@gmail.com>
2024-05-15 15:58:56 +0300

    modified app.js to pretect the profile page for only authenticated users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   useLocation,
+  Navigate,
 } from "react-router-dom";
 import "./App.css";
 import "flowbite";
@@ -15,14 +16,39 @@ import ProfilePage from "./components/profile_page";
 import Skills from "./components/skills";
 import Tutors from "./components/tutors";
 import Footer from "./components/footer";
-
-import Team from "./components/team";
-
 import axios from "axios";
+
 axios.defaults.withCredentials = true;
 
+const ProtectedRoute = ({ component: Component }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const location = useLocation();
 
- 
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/session");
+        setIsAuthenticated(response.data.isAuthenticated);
+      } catch (error) {
+        console.error("Authentication check failed", error);
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>; // or some other loading state if desired
+  }
+
+  return isAuthenticated ? (
+    <Component />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+};
+
 const App = () => {
   return (
     <Router>
@@ -34,7 +60,9 @@ const App = () => {
 const AppContent = () => {
   const location = useLocation();
   const showHeader =
-    location.pathname === "/" || location.pathname === "/profile" || location.pathname === "/topics";
+    location.pathname === "/" ||
+    location.pathname === "/profile" ||
+    location.pathname === "/topics";
 
   return (
     <>
@@ -43,8 +71,11 @@ const AppContent = () => {
         <Route path="/" element={<HeroSection />} />
         <Route path="/login" element={<SignInCard />} />
         <Route path="/register" element={<RegisterForm />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/topics" element={<Skills />} /> 
+        <Route
+          path="/profile"
+          element={<ProtectedRoute component={ProfilePage} />}
+        />
+        <Route path="/topics" element={<Skills />} />
         <Route path="/tutors" element={<Tutors />} />
       </Routes>
       <Footer />
