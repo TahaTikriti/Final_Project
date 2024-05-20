bc74950434be5fa8a907c7f6b1ae47a6e186d6a6
Rabih-1 <Rabihkouzayha1@gmail.com>
2024-05-20 19:57:01 +0300

    location field in editProfile (updated to select options)

--- frontend/src/components/editProfile.js
+++ frontend/src/components/editProfile.js
@@ -88,9 +88,24 @@ function EditProfile({ closeEdit }) {
                             <textarea id="bio" name="bio" value={formData.bio} onChange={handleChange} rows="3" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text:white" placeholder="Write your bio here"></textarea>
                         </div>
                         <div className="sm:col-span-2">
-                            <label htmlFor="location" className="block mb-2 text-sm font-medium text-gray-900 dark:text:white">Location</label>
-                            <input type="text" name="location" id="location" value={formData.location} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text:white" placeholder="Enter your location" />
-                        </div>
+  <label htmlFor="location" className="block mb-2 text-sm font-medium text-gray-900 dark:text:white">
+    Location
+  </label>
+  <select
+    name="location"
+    id="location"
+    value={formData.location}
+    onChange={handleChange}
+    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text:white"
+  >
+    <option value="">Select your location</option>
+    <option value="TRIPOLI">TRIPOLI</option>
+    <option value="BEIRUT">BEIRUT</option>
+    <option value="SAIDA">SAIDA</option>
+    <option value="SOUR">SOUR</option>
+  </select>
+</div>
+
                         {formData.skills.map((skill, index) => (
                             <div key={index} className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-2">
                                 <input 
