c686a016a83b833cf65554a2d13e0694640c80c6
Rabih-1 <Rabihkouzayha1@gmail.com>
2024-05-20 03:08:29 +0300

    reload the /profile while i click on the save changes button in edit profile component

--- frontend/src/components/editProfile.js
+++ frontend/src/components/editProfile.js
@@ -133,9 +133,14 @@ function EditProfile({ closeEdit }) {
                         </button>
                         {/* Close and Save buttons */}
                         <div className="flex justify-end gap-4">
-                            <button type="submit" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center">
-                                Save Changes
-                            </button>
+                        <button 
+    type="submit" 
+    className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center"
+    onClick={() => window.location.href = '/profile'}
+>
+    Save Changes
+</button>
+
                             <button type="button" className="bg-gray-500 hover:bg-gray-600 text-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-lg text-sm px-5 py-2.5 inline-flex items-center" onClick={closeEdit}>
                                 Close
                             </button>
